0f39a8a0dc412e1b6cf65d99fd87afb5e10fdc26
Amit Baghel <amit.baghel.agra@gmail.com>
2025-09-13 18:08:33 +0530

    new script

---
+++ create-dir-structure.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash bash
+
+# structure menu
+
+function structure_menu_a() {
+    clear
+    echo "------------------------------------------"
+    echo "         STRUCTURE MENU - A"
+    echo "------------------------------------------"
+    echo "1) DB"
+    echo "2) DB + Patch"
+    echo "q) Quit"
+    echo "------------------------------------------"
+    echo -n "Enter your choice: "
+}
+
+function structure_menu_b() {
+    clear
+    echo "------------------------------------------"
+    echo "         STRUCTURE MENU - B"
+    echo "------------------------------------------"
+    echo "1) KNIME"
+    echo "2) Python"
+    echo "q) Quit"
+    echo "------------------------------------------"
+    echo -n "Enter your choice: "
+}
+
+# DB deployment 
+# DB + Patch deployment
+# KNIME deployment
+# python deployment
